8f8c0906f1f60ec8487e40aab9023d517ff602e6
Chris Walter <walt50@gmail.com>
2023-01-10 09:19:49 -0500

    Initial Commit

---
+++ Bracketyzer/BracketyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BracketyzerApp.swift
+//  Bracketyzer
+//
+//  Created by Christopher Walter on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BracketyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
